b943d1dd8e36e75e8ccaf6593745964cddc2f38e
snsten <>
2020-07-09 17:08:00 +0530

    Rebase flask based on another project

--- app.py
+++ app.py
@@ -1,109 +1,111 @@
 import os
-import model
-import flask
+import sys
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
 from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
 
-# Creating a new Flask Web application. It accepts the package name.
-app = flask.Flask(__name__)
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Preprocessing utilities
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Model building
+from keras import layers
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras.applications import DenseNet121
+from keras.callbacks import Callback, ModelCheckpoint
 
 
-def CNN_predict():
+from PIL import Image
+from models.model import build_model, preprocess_image
+
+# Some utilites
+import numpy as np
+from utils import base64_to_pil
+
+
+# Creating a new Flask Web application.
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = './models/pretrained/model.h5'
+
+# Loading trained model
+model = build_model()
+model.load_weights(MODEL_PATH)
+print('Model loaded. Start serving...')
+
+
+def model_predict(img, model):
     """
-    Upload the image file and classify the severity of DR using pre-trained CNN model
-    """
+    Classify the severity of DR of image using pre-trained CNN model.
 
-    """
-    Predicted rating of image for the severity of diabetic retinopathy on a scale of 0 to 4:
+    Keyword arguments:
+    img -- the retinal image to be classified
+    model -- the pretrained CNN model used for prediction
+
+    Predicted rating of severity of diabetic retinopathy on a scale of 0 to 4:
 
     0 - No DR
     1 - Mild
     2 - Moderate
     3 - Severe
     4 - Proliferative DR
-    """
-    diagnosis = ["No DR", "Mild", "Moderate", "Severe", "Proliferative DR"]
-
-    global secure_filename
-
-    # Getting the image path
-    img_path = os.path.join(app.root_path, secure_filename)
 
     """
-    Check if image exists
-    """
-    if os.path.isfile(img_path):
-        # predicting class from pretrained model
-        predicted_class = model.classify_image(img_path)
+    
+    ## Preprocessing the image
+    x_val = np.empty((1, 224, 224, 3), dtype=np.uint8)
+    img = img.resize((224,) * 2, resample=Image.LANCZOS)
+    x_val[0, :, :, :] = img
 
-        """
-        After predicting the class label of the input image, 
-        the prediction label and predicted diagnosis are rendered on an HTML page.
-        The HTML page is fetched from the /templates directory. 
-        The HTML page accepts two inputs which are the predicted class and predicted diagnosis.
-        """
-        return flask.render_template(
-            template_name_or_list="prediction_result.html",
-            predicted_class=predicted_class,
-            predicted_diagnosis=diagnosis[predicted_class],
-        )
-    else:
-        # The error page is shown if file is not found
-        return flask.render_template(template_name_or_list="error.html")
+    preds = model.predict(x_val)
+    return preds
 
 
-app.add_url_rule(rule="/predict/", endpoint="predict", view_func=CNN_predict)
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
 
 
-def upload_image():
-    """
-    Viewer function that is called in response to getting to the 'http://localhost:port/upload' URL.
-    It uploads the selected image to the server.
-    :return: redirects the application to a new page for predicting the class of the image.
-    """
-    # Global variable to hold the name of the image file for reuse later in prediction by the 'CNN_predict' viewer functions.
-    global secure_filename
-    if (
-        flask.request.method == "POST"
-    ):  # Checking of the HTTP method initiating the request is POST.
-        img_file = flask.request.files[
-            "image_file"
-        ]  # Getting the file name to get uploaded.
-        secure_filename = secure_filename(
-            img_file.filename
-        )  # Getting a secure file name. It is a good practice to use it.
-        img_path = os.path.join(
-            app.root_path, secure_filename
-        )  # Preparing the full path under which the image will get saved.
-        img_file.save(img_path)  # Saving the image in the specified path.
-        print("Image uploaded successfully.")
-        """
-        After uploading the image file successfully, next is to predict the class label of it.
-        The application will fetch the URL that is tied to the HTML page responsible for prediction and redirects the browser to it.
-        The URL is fetched using the endpoint 'predict'.
-        """
-        return flask.redirect(flask.url_for(endpoint="predict"))
-    return "Image upload failed."
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+
+        # Save the image to ./uploads
+        # img.save("./uploads/image.png")
+
+        # Make prediction on the image
+        preds = model_predict(img, model)
+
+        # Process result to find probability and class of prediction
+        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
+        pred_class = np.argmax(np.squeeze(preds))
+
+        diagnosis = ["No DR", "Mild", "Moderate", "Severe", "Proliferative DR"]
+
+        result = diagnosis[pred_class]               # Convert to string
+        
+        # Serialize the result
+        return jsonify(result=result, probability=pred_proba)
+
+    return None
 
 
-app.add_url_rule(
-    rule="/upload/", endpoint="upload", view_func=upload_image, methods=["POST"]
-)
+if __name__ == '__main__':
+    # app.run(port=5002, threaded=False)
 
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
 
-def redirect_upload():
-    return flask.render_template(template_name_or_list="upload_image.html")
-
-
-app.add_url_rule(rule="/", endpoint="homepage", view_func=redirect_upload)
-
-"""
-To activate the Web server to receive requests, the application must run from the main method.
-"""
-if __name__ == "__main__":
-    """
-    In this example, the app will run based on the following properties:
-    host: localhost
-    port: 5000 
-    debug: flag set to True to return debugging information.
-    """
-    app.run(host="localhost", port=5000, debug=True)
--- static/result.js
+++
@@ -1,4 +0,0 @@
-function show_alert(predicted_class){
-alert("Processing Finished.\nPredicted class is *"+predicted_class+"*.")
-alert("Processing Finished.\nPredicted Diagnosis is *"+predicted_diagnosis+"*.")
-}
